8c0f6b7cedd93d21caefa386160d2cb0a317a061
ankit <ankitkuswaha07@gmail.com>
2023-11-27 19:33:47 +0530

    render a page from ejs

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const ejs = require("ejs")
+const path = require("path")
+
+
+
+const expressEjsLayouts = require("express-ejs-layouts")
+
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+
+
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+
+
+// set Termplate engine
+app.use(expressEjsLayouts)
+app.set('views',path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+
+app.listen(PORT,()=>{
+    console.log(`listening on port ${PORT}`)
+})
+

1ce8ad24f4379f9d816770985de4e65a66516188
ankit <ankitkuswaha07@gmail.com>
2023-11-28 22:22:16 +0530

    configure laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,4 @@
 
 let mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js').sass('resources/sass/app.scss','public/css/app.css');
+mix.js('resources/js/app.js', 'public/js/app.js').sass('resources/scss/app.scss','public/css/app.css');

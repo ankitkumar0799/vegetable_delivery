e2df8a76ddf95fa066ed4a3da90197a178291be5
ankit <ankitkuswaha07@gmail.com>
2023-12-02 15:28:24 +0530

    add expresslayouts and reduce repetition code

--- server.js
+++ server.js
@@ -13,11 +13,6 @@ const PORT = process.env.PORT || 3000
 
 
 
-// assets
-app.use(express.static('public'))
-app.get('/',(req,res)=>{
-    res.render('home')
-})
 
 
 
@@ -25,6 +20,16 @@ app.get('/',(req,res)=>{
 app.use(expressEjsLayouts)
 app.set('views',path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
+
+
+// assets web pages by get
+app.use(express.static('public'))
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart')
+})
 
 
 app.listen(PORT,()=>{

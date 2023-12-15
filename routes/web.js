ebea5bfc19c065eaa7cfa2e243420ad106c14c74
ankit <ankitkuswaha07@gmail.com>
2023-12-15 23:32:36 +0530

    connect data base by mongoose using mongodb

--- routes/web.js
+++ routes/web.js
@@ -1,15 +1,10 @@
 const homeController = require("../app/http/controllers/homeController")
 const authController = require("../app/http/controllers/authController")
-
-
+const cartController = require("../app/http/controllers/customers/cartController")
 function initRoutes(app) {
     app.get('/',homeController().index)
-
-    app.get('/cart',(req,res)=>{
-        res.render('customers/cart')
-    })
+    app.get('/cart',cartController().cart)
     app.get('/',authController().login)
-    
     app.get('/register',authController().register)
 }
 module.exports = initRoutes
--- server.js
+++ server.js
@@ -1,39 +1,39 @@
-const express = require("express")
-const ejs = require("ejs")
-const path = require("path")
+const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+const mongoose = require("mongoose");
+const expressEjsLayouts = require("express-ejs-layouts");
+
+// database connection here===>
+const url = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 
 
+mongoose
+  .connect(url, {
+    dbName: "vegitable",
+    useNewUrlParser: true,
+    
+    useUnifiedTopology: true,
+    
+  })
+  .then((c) => console.log(`database is connected`))
+  .catch((e) => console.log(e));
 
-const expressEjsLayouts = require("express-ejs-layouts")
+const app = express();
 
-
-const app = express()
-
-const PORT = process.env.PORT || 3000
-
-
-
-
-
+const PORT = process.env.PORT || 3000;
 
 // set Termplate engine
-app.use(expressEjsLayouts)
-app.set('views',path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
-
+app.use(expressEjsLayouts);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
 
 // assets web pages by get
-app.use(express.static('public'))
-
+app.use(express.static("public"));
 
 // routes
-require('./routes/web')(app)
+require("./routes/web")(app);
 
-
-
-
-
-app.listen(PORT,()=>{
-    console.log(`listening on port ${PORT}`)
-})
-
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
